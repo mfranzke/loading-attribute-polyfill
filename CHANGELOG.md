e7e799b46973a2968cea3492c3f0578a1d1bf166
Cyril Krylatov <cyril.krylatov+github@gmail.com>
2020-02-27 14:40:53 +0100

    chore: export lazyload function

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,12 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 And the commit messages from [Conventional Commits](https://conventionalcommits.org) are being used.
 
+## [1.5.0] - 2020-02-27
+
+### Changed
+
+- Add an export logic to reload the function anytime we want
+
 ## [1.4.1] - 2020-01-25
 
 ### Changed
--- loading-attribute-polyfill.js
+++ loading-attribute-polyfill.js
@@ -9,253 +9,257 @@
  * Elsewhere the functionality gets emulated with the support of noscript wrapper tags.
  * Use an IntersectionObserver polyfill in case of IE11 support necessary.
  */
+const lazyload = function (noscriptClass, rootMargin) {
+    'use strict';
 
-(function(noscriptClass, rootMargin) {
-	'use strict';
+    var config = {
+        // Start download if the item gets within 256px in the Y axis
+        rootMargin: rootMargin || '0px 0px 256px 0px',
+        threshold: 0.01,
+        lazyImage: 'img[loading="lazy"]',
+        lazyIframe: 'iframe[loading="lazy"]',
+        noscriptClass: noscriptClass || 'loading-lazy',
+    };
 
-	var config = {
-		// Start download if the item gets within 256px in the Y axis
-		rootMargin: rootMargin || '0px 0px 256px 0px',
-		threshold: 0.01,
-		lazyImage: 'img[loading="lazy"]',
-		lazyIframe: 'iframe[loading="lazy"]'
-	};
+    // Device/browser capabilities object
+    var capabilities = {
+        loading:
+            'loading' in HTMLImageElement.prototype &&
+            'loading' in HTMLIFrameElement.prototype,
+        scrolling: 'onscroll' in window
+    };
 
-	// Device/browser capabilities object
-	var capabilities = {
-		loading:
-			'loading' in HTMLImageElement.prototype &&
-			'loading' in HTMLIFrameElement.prototype,
-		scrolling: 'onscroll' in window
-	};
+    // Nodelist foreach polyfill / source: https://stackoverflow.com/a/46929259
+    if (
+        typeof NodeList !== 'undefined' &&
+        NodeList.prototype &&
+        !NodeList.prototype.forEach
+    ) {
+        // Yes, there's really no need for `Object.defineProperty` here
+        NodeList.prototype.forEach = Array.prototype.forEach;
+    }
 
-	// Nodelist foreach polyfill / source: https://stackoverflow.com/a/46929259
-	if (
-		typeof NodeList !== 'undefined' &&
-		NodeList.prototype &&
-		!NodeList.prototype.forEach
-	) {
-		// Yes, there's really no need for `Object.defineProperty` here
-		NodeList.prototype.forEach = Array.prototype.forEach;
-	}
+    // Define according to browsers support of the IntersectionObserver feature (missing e.g. on IE11 or Safari 11)
+    var intersectionObserver;
 
-	// Define according to browsers support of the IntersectionObserver feature (missing e.g. on IE11 or Safari 11)
-	var intersectionObserver;
+    if ('IntersectionObserver' in window) {
+        intersectionObserver = new IntersectionObserver(onIntersection, config);
+    }
 
-	if ('IntersectionObserver' in window) {
-		intersectionObserver = new IntersectionObserver(onIntersection, config);
-	}
+    // On using a browser w/o requestAnimationFrame support (IE9, Opera Mini), just run the passed function
+    var rAFWrapper;
 
-	// On using a browser w/o requestAnimationFrame support (IE9, Opera Mini), just run the passed function
-	var rAFWrapper;
+    if ('requestAnimationFrame' in window) {
+        rAFWrapper = window.requestAnimationFrame;
+    } else {
+        rAFWrapper = function(func) {
+            func();
+        };
+    }
 
-	if ('requestAnimationFrame' in window) {
-		rAFWrapper = window.requestAnimationFrame;
-	} else {
-		rAFWrapper = function(func) {
-			func();
-		};
-	}
+    /**
+     * Put the source and srcset back where it belongs - now that the elements content is attached to the document, it will load now
+     * @param {Object} lazyItem Current item to be restored after lazy loading.
+     */
+    function restoreSource(lazyItem) {
+        var srcsetItems = [];
 
-	/**
-	 * Put the source and srcset back where it belongs - now that the elements content is attached to the document, it will load now
-	 * @param {Object} lazyItem Current item to be restored after lazy loading.
-	 */
-	function restoreSource(lazyItem) {
-		var srcsetItems = [];
+        // Just in case the img is the decendent of a picture element, check for source tags
+        if (lazyItem.parentNode.tagName.toLowerCase() === 'picture') {
+            removePlaceholderSource(lazyItem.parentNode);
 
-		// Just in case the img is the decendent of a picture element, check for source tags
-		if (lazyItem.parentNode.tagName.toLowerCase() === 'picture') {
-			removePlaceholderSource(lazyItem.parentNode);
+            srcsetItems = Array.prototype.slice.call(
+                lazyItem.parentNode.querySelectorAll('source')
+            );
+        }
 
-			srcsetItems = Array.prototype.slice.call(
-				lazyItem.parentNode.querySelectorAll('source')
-			);
-		}
+        srcsetItems.push(lazyItem);
 
-		srcsetItems.push(lazyItem);
+        srcsetItems.forEach(function(item) {
+            if (item.dataset.lazySrcset) {
+                item.setAttribute('srcset', item.dataset.lazySrcset);
+                delete item.dataset.lazySrcset;
+            }
+        });
 
-		srcsetItems.forEach(function(item) {
-			if (item.dataset.lazySrcset) {
-				item.setAttribute('srcset', item.dataset.lazySrcset);
-				delete item.dataset.lazySrcset;
-			}
-		});
+        lazyItem.setAttribute('src', lazyItem.dataset.lazySrc);
+        delete lazyItem.dataset.lazySrc;
+    }
 
-		lazyItem.setAttribute('src', lazyItem.dataset.lazySrc);
-		delete lazyItem.dataset.lazySrc;
-	}
+    /**
+     * Remove the source tag preventing the loading of picture assets
+     * @param {Object} lazyItemPicture Current <picture> item to be restored after lazy loading.
+     */
+    function removePlaceholderSource(lazyItemPicture) {
+        var placeholderSource = lazyItemPicture.querySelector(
+            'source[data-lazy-remove]'
+        );
 
-	/**
-	 * Remove the source tag preventing the loading of picture assets
-	 * @param {Object} lazyItemPicture Current <picture> item to be restored after lazy loading.
-	 */
-	function removePlaceholderSource(lazyItemPicture) {
-		var placeholderSource = lazyItemPicture.querySelector(
-			'source[data-lazy-remove]'
-		);
+        if (placeholderSource) {
+            lazyItemPicture.removeChild(placeholderSource); // Preferred .removeChild over .remove here for IE
+        }
+    }
 
-		if (placeholderSource) {
-			lazyItemPicture.removeChild(placeholderSource); // Preferred .removeChild over .remove here for IE
-		}
-	}
+    /**
+     * Handle IntersectionObservers callback
+     * @param {Object} entries Target elements Intersection observed changes
+     * @param {Object} observer IntersectionObserver instance reference
+     */
+    function onIntersection(entries, observer) {
+        entries.forEach(function(entry) {
+            // Mitigation for EDGE lacking support of .isIntersecting until v15, compare to e.g. https://github.com/w3c/IntersectionObserver/issues/211#issuecomment-309144669
+            if (entry.intersectionRatio === 0) {
+                return;
+            }
 
-	/**
-	 * Handle IntersectionObservers callback
-	 * @param {Object} entries Target elements Intersection observed changes
-	 * @param {Object} observer IntersectionObserver instance reference
-	 */
-	function onIntersection(entries, observer) {
-		entries.forEach(function(entry) {
-			// Mitigation for EDGE lacking support of .isIntersecting until v15, compare to e.g. https://github.com/w3c/IntersectionObserver/issues/211#issuecomment-309144669
-			if (entry.intersectionRatio === 0) {
-				return;
-			}
+            // If the item is visible now, load it and stop watching it
+            var lazyItem = entry.target;
 
-			// If the item is visible now, load it and stop watching it
-			var lazyItem = entry.target;
+            observer.unobserve(lazyItem);
 
-			observer.unobserve(lazyItem);
+            restoreSource(lazyItem);
+        });
+    }
 
-			restoreSource(lazyItem);
-		});
-	}
+    /**
+     * Handle printing the page
+     */
+    function onPrinting() {
+        if (typeof window.matchMedia === 'undefined') {
+            return;
+        }
 
-	/**
-	 * Handle printing the page
-	 */
-	function onPrinting() {
-		if (typeof window.matchMedia === 'undefined') {
-			return;
-		}
+        var mediaQueryList = window.matchMedia('print');
 
-		var mediaQueryList = window.matchMedia('print');
+        mediaQueryList.addListener(function(mql) {
+            if (mql.matches) {
+                document
+                    .querySelectorAll(
+                        config.lazyImage +
+                            '[data-lazy-src],' +
+                            config.lazyIframe +
+                            '[data-lazy-src]'
+                    )
+                    .forEach(function(lazyItem) {
+                        restoreSource(lazyItem);
+                    });
+            }
+        });
+    }
 
-		mediaQueryList.addListener(function(mql) {
-			if (mql.matches) {
-				document
-					.querySelectorAll(
-						config.lazyImage +
-							'[data-lazy-src],' +
-							config.lazyIframe +
-							'[data-lazy-src]'
-					)
-					.forEach(function(lazyItem) {
-						restoreSource(lazyItem);
-					});
-			}
-		});
-	}
+    /**
+     * Get and prepare the HTML code depending on feature detection for both image as well as iframe,
+     * and if not scrolling supported, because it's a Google or Bing Bot
+     * @param {String} lazyAreaHtml Noscript inner HTML code that src-urls need to get rewritten
+     */
+    function getAndPrepareHTMLCode(noScriptTag) {
+        // The contents of a <noscript> tag are treated as text to JavaScript
+        var lazyAreaHtml = noScriptTag.textContent || noScriptTag.innerHTML;
 
-	/**
-	 * Get and prepare the HTML code depending on feature detection for both image as well as iframe,
-	 * and if not scrolling supported, because it's a Google or Bing Bot
-	 * @param {String} lazyAreaHtml Noscript inner HTML code that src-urls need to get rewritten
-	 */
-	function getAndPrepareHTMLCode(noScriptTag) {
-		// The contents of a <noscript> tag are treated as text to JavaScript
-		var lazyAreaHtml = noScriptTag.textContent || noScriptTag.innerHTML;
+        var getImageWidth = lazyAreaHtml.match(/width=[\'\"]([0-9]+)[\'\"]/) || false;
+        var temporaryImageWidth = getImageWidth[1] || 1;
+        var getImageHeight = lazyAreaHtml.match(/height=[\'\"]([0-9]+)[\'\"]/) || false;
+        var temporaryImageHeight = getImageHeight[1] || 1;
 
-		var getImageWidth = lazyAreaHtml.match(/width=[\'\"]([0-9]+)[\'\"]/) || false;
-		var temporaryImageWidth = getImageWidth[1] || 1;
-		var getImageHeight = lazyAreaHtml.match(/height=[\'\"]([0-9]+)[\'\"]/) || false;
-		var temporaryImageHeight = getImageHeight[1] || 1;
+        var temporaryImage = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 " + temporaryImageWidth + " " + temporaryImageHeight + "'%3E%3C/svg%3E";
 
-		var temporaryImage = "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 " + temporaryImageWidth + " " + temporaryImageHeight + "'%3E%3C/svg%3E";
+        if (
+            !capabilities.loading &&
+            capabilities.scrolling &&
+            typeof document.documentElement.dataset !== 'undefined'
+        ) {
+            // Check for IntersectionObserver support
+            if (typeof intersectionObserver === 'undefined') {
+                // Attach abandonned attribute 'lazyload' to the HTML tags on browsers w/o IntersectionObserver being available
+                lazyAreaHtml = lazyAreaHtml.replace(
+                    /(?:\r\n|\r|\n|\t| )src=/g,
+                    ' lazyload="1" src='
+                );
+            } else {
+                if (noScriptTag.parentNode.tagName.toLowerCase() === 'picture') {
+                    // Temporarily prevent expensive resource loading by inserting a <source> tag pointing to a simple one (data URI)
+                    lazyAreaHtml =
+                        '<source srcset="' +
+                        temporaryImage +
+                        '" data-lazy-remove="true"></source>' +
+                        lazyAreaHtml;
+                }
 
-		if (
-			!capabilities.loading &&
-			capabilities.scrolling &&
-			typeof document.documentElement.dataset !== 'undefined'
-		) {
-			// Check for IntersectionObserver support
-			if (typeof intersectionObserver === 'undefined') {
-				// Attach abandonned attribute 'lazyload' to the HTML tags on browsers w/o IntersectionObserver being available
-				lazyAreaHtml = lazyAreaHtml.replace(
-					/(?:\r\n|\r|\n|\t| )src=/g,
-					' lazyload="1" src='
-				);
-			} else {
-				if (noScriptTag.parentNode.tagName.toLowerCase() === 'picture') {
-					// Temporarily prevent expensive resource loading by inserting a <source> tag pointing to a simple one (data URI)
-					lazyAreaHtml =
-						'<source srcset="' +
-						temporaryImage +
-						'" data-lazy-remove="true"></source>' +
-						lazyAreaHtml;
-				}
+                // Temporarily replace a expensive resource load with a simple one by storing the actual source and srcset for later and point src to a temporary replacement (data URI)
+                lazyAreaHtml = lazyAreaHtml
+                    .replace(/(?:\r\n|\r|\n|\t| )srcset=/g, ' data-lazy-srcset=')
+                    .replace(
+                        /(?:\r\n|\r|\n|\t| )src=/g,
+                        ' src="' + temporaryImage + '" data-lazy-src='
+                    );
+            }
+        }
 
-				// Temporarily replace a expensive resource load with a simple one by storing the actual source and srcset for later and point src to a temporary replacement (data URI)
-				lazyAreaHtml = lazyAreaHtml
-					.replace(/(?:\r\n|\r|\n|\t| )srcset=/g, ' data-lazy-srcset=')
-					.replace(
-						/(?:\r\n|\r|\n|\t| )src=/g,
-						' src="' + temporaryImage + '" data-lazy-src='
-					);
-			}
-		}
+        return lazyAreaHtml;
+    }
 
-		return lazyAreaHtml;
-	}
+    /**
+     * Retrieve the elements from the 'lazy load' <noscript> tag and prepare them for display
+     * @param {Object} noScriptTag noscript HTML tag that should get initially transformed
+     */
+    function prepareElement(noScriptTag) {
+        // Sticking the noscript HTML code in the innerHTML of a new <div> tag to 'load' it after creating that <div>
+        var lazyArea = document.createElement('div');
 
-	/**
-	 * Retrieve the elements from the 'lazy load' <noscript> tag and prepare them for display
-	 * @param {Object} noScriptTag noscript HTML tag that should get initially transformed
-	 */
-	function prepareElement(noScriptTag) {
-		// Sticking the noscript HTML code in the innerHTML of a new <div> tag to 'load' it after creating that <div>
-		var lazyArea = document.createElement('div');
+        lazyArea.innerHTML = getAndPrepareHTMLCode(noScriptTag);
 
-		lazyArea.innerHTML = getAndPrepareHTMLCode(noScriptTag);
+        // Move all children out of the element
+        while (lazyArea.firstChild) {
+            if (
+                !capabilities.loading &&
+                capabilities.scrolling &&
+                typeof intersectionObserver !== 'undefined' &&
+                typeof document.documentElement.dataset !== 'undefined' &&
+                lazyArea.firstChild.tagName &&
+                (lazyArea.firstChild.tagName.toLowerCase() === 'img' ||
+                    lazyArea.firstChild.tagName.toLowerCase() === 'iframe')
+            ) {
+                // Observe the item so that loading could start when it gets close to the viewport
+                intersectionObserver.observe(lazyArea.firstChild);
+            }
 
-		// Move all children out of the element
-		while (lazyArea.firstChild) {
-			if (
-				!capabilities.loading &&
-				capabilities.scrolling &&
-				typeof intersectionObserver !== 'undefined' &&
-				typeof document.documentElement.dataset !== 'undefined' &&
-				lazyArea.firstChild.tagName &&
-				(lazyArea.firstChild.tagName.toLowerCase() === 'img' ||
-					lazyArea.firstChild.tagName.toLowerCase() === 'iframe')
-			) {
-				// Observe the item so that loading could start when it gets close to the viewport
-				intersectionObserver.observe(lazyArea.firstChild);
-			}
+            noScriptTag.parentNode.insertBefore(lazyArea.firstChild, noScriptTag);
+        }
 
-			noScriptTag.parentNode.insertBefore(lazyArea.firstChild, noScriptTag);
-		}
+        // Remove the empty element - not using .remove() here for IE11 compatibility
+        noScriptTag.parentNode.removeChild(noScriptTag); // Preferred .removeChild over .remove here for IE
+    }
 
-		// Remove the empty element - not using .remove() here for IE11 compatibility
-		noScriptTag.parentNode.removeChild(noScriptTag); // Preferred .removeChild over .remove here for IE
-	}
+    /**
+     * Get all the <noscript> tags on the page and setup the printing
+     */
+    function prepareElements() {
+        //
+        var lazyLoadAreas = document.querySelectorAll('noscript.' + config.noscriptClass);
 
-	/**
-	 * Get all the <noscript> tags on the page and setup the printing
-	 */
-	function prepareElements() {
-		//
-		var lazyLoadAreas = document.querySelectorAll('noscript.' + noscriptClass);
+        lazyLoadAreas.forEach(prepareElement);
 
-		lazyLoadAreas.forEach(prepareElement);
+        // Bind for someone printing the page
+        onPrinting();
+    }
 
-		// Bind for someone printing the page
-		onPrinting();
-	}
+    // If the page has loaded already, run setup - if it hasn't, run as soon as it has.
+    // Use requestAnimationFrame as this will propably cause repaints
+    if (/comp|inter/.test(document.readyState)) {
+        rAFWrapper(prepareElements);
+    } else if ('addEventListener' in document) {
+        document.addEventListener('DOMContentLoaded', function() {
+            rAFWrapper(prepareElements);
+        });
+    } else {
+        document.attachEvent('onreadystatechange', function() {
+            if (document.readyState === 'complete') {
+                prepareElements();
+            }
+        });
+    }
+};
 
-	// If the page has loaded already, run setup - if it hasn't, run as soon as it has.
-	// Use requestAnimationFrame as this will propably cause repaints
-	if (/comp|inter/.test(document.readyState)) {
-		rAFWrapper(prepareElements);
-	} else if ('addEventListener' in document) {
-		document.addEventListener('DOMContentLoaded', function() {
-			rAFWrapper(prepareElements);
-		});
-	} else {
-		document.attachEvent('onreadystatechange', function() {
-			if (document.readyState === 'complete') {
-				prepareElements();
-			}
-		});
-	}
-}('loading-lazy', '256px 0px'));
+lazyload('loading-lazy', '256px 0px');
+
+export default lazyload;
