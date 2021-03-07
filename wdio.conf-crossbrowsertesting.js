3040fd19b87620072460794714f6d4d7a3ed2fc4
Maximilian <github-public-address-20201011-mfr@nzke.net>
2021-03-07 11:20:29 +0100

    refactor(test): removed unnecessary dependency

--- wdio.conf-crossbrowsertesting.js
+++ wdio.conf-crossbrowsertesting.js
@@ -205,7 +205,7 @@ exports.config = {
 	// Services take over a specific job you don't want to take care of. They enhance
 	// your test setup with almost no effort. Unlike plugins, they don't add new
 	// commands. Instead, they hook themselves up into the test process.
-	services: ['crossbrowsertesting'],
+	services: [],
 	// Framework you want to run your specs with.
 	// The following are supported: Mocha, Jasmine, and Cucumber
 	// see also: https://webdriver.io/docs/frameworks.html
